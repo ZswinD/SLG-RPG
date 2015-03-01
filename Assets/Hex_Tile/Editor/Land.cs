2dcece741573db78332423ddfbe67bcd2aa867eb
o2yCN <412843941@qq.com>
2015-03-01 12:31:07 +0800

    123

--- Assets/Hex_Tile/Editor/Land.cs
+++ Assets/Hex_Tile/Editor/Land.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 
 public class Land:EditorWindow
 {
+	public string s="Hello World";
 	[MenuItem("HexGrids/Land")]
 	static void ShowLand(){
-		Rect wr = new Rect (0, 0, 500, 500);
+		Rect wr = new Rect (0, 0, 200, 300);
 		Land window = (Land)EditorWindow.GetWindowWithRect <Land> (wr, false, "Lands");
 		window.Show ();
+	}
+	void OnGUI()
+	{
+		GUILayout.Label ("Land");
+		s = GUILayout.TextField (s);
+		if (GUILayout.Button ("Debug"))
+						Debug.Log (s);
 	}
 }
